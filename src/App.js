cb39848bc1349e734f94dc4aef176f29886343e5
yoshiodei <yoshiodei@yahoo.com>
2022-08-23 23:11:17 +0000

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import JoinChat from './JoinChat.jsx';
 import './Test.js';
 import io from 'socket.io-client';
+import ChatPage from './chatPage/index.js';
+import { Route, Routes } from 'react-router-dom';
+import SignUp from './authPage/SignUp.jsx';
+import SignIn from './authPage/SignIn.jsx';
 
-const socket = io.connect("http://localhost:4000"); 
+// const socket = io.connect("http://localhost:4000"); 
 
 function App() {
   return (
     <div className="App">
-      <JoinChat socket={socket} />
+       <Routes>
+        <Route path='/' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/:userId' element={<ChatPage />} />
+      </Routes> 
     </div>
   );
 }
