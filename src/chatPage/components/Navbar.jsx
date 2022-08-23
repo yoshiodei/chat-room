719fee6f3059e8db233d838da3da4229215a9238
yoshiodei <yoshiodei@yahoo.com>
2022-08-23 23:10:32 +0000

    create navbar component and styles

---
+++ src/chatPage/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+const Navbar = () => {
+    return (
+        <header className='navbar'>
+            <h2>kazu.Social</h2>
+        </header>
+    );
+}
+
+export default Navbar;
